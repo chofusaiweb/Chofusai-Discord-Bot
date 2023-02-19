ff856f229469916fd8079caf072c42a029519e3e
sushi-chaaaan <mail@sushichan.live>
2023-02-20 00:58:22 +0900

    Add message dispander

---
+++ src/cogs/dispand.py
@@ -0,0 +1,34 @@
+from typing import TYPE_CHECKING
+
+import discord
+from discord.ext import commands
+from dispander import dispand
+
+if TYPE_CHECKING:
+    # import some original class
+    from src.bot import Bot
+
+    pass
+
+
+class Dispander(commands.Cog):
+    def __init__(self, bot: "Bot"):
+        self.bot = bot
+
+    @commands.Cog.listener("on_message")
+    async def on_message(self, message: discord.Message):
+        if message.author.bot:
+            return
+
+        embeds: list[discord.Embed] = await dispand(self.bot, message)
+
+        if embeds:
+            try:
+                await message.channel.send(embeds=embeds)
+            except Exception as e:
+                self.bot.logger.error(e)
+            return
+
+
+async def setup(bot: "Bot"):
+    await bot.add_cog(Dispander(bot))
